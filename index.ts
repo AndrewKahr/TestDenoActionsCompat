7aa012e9726af205ed43e5586da2ac2290269a1e
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-04-04 18:22:56 -0700

    Debug log

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ import { saveCache, restoreCache } from 'npm:@actions/cache@3.2.1';
 
 Deno.writeTextFileSync('cache.txt', 'Hello World');
 
+console.log(Deno.env.get('ACTIONS_CACHE_URL'));
+
 saveCache(['cache.txt'], 'cache-key');
